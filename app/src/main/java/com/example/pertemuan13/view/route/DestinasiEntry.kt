49b8c63fa4bb2c46bda9474c38ebca031631b2fa
RizkyPratama05 <rizkypp428@gmail.com>
2026-01-02 21:12:02 +0700

    membuat file DestinasiEntry.

---
+++ app/src/main/java/com/example/pertemuan13/view/route/DestinasiEntry.kt
@@ -0,0 +1,9 @@
+package com.example.pertemuan13.view.route
+
+import com.example.pertemuan13.R
+import com.example.pertemuan13.view.route.DestinasiNavigasi
+
+object DestinasiEntry : DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+}
