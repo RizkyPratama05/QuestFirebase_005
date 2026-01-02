7365d1e218e96343d2eb06f541194f4b4846fc1f
RizkyPratama05 <rizkypp428@gmail.com>
2026-01-02 21:12:21 +0700

    membuat file DestinasiHome.kt

---
+++ app/src/main/java/com/example/pertemuan13/view/route/DestinasiHome.kt
@@ -0,0 +1,9 @@
+package com.example.pertemuan13.view.route
+
+import com.example.pertemuan13.R
+import com.example.pertemuan13.view.route.DestinasiNavigasi
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
